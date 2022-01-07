119755a23011af52bd6d8bb78d7d2e28c39c0b93
Cosmin Popovici <hellocosmin@gmail.com>
2022-01-07 13:49:59 +0200

    feat: do nothing if url was not provided

--- test/test.js
+++ test/test.js
@@ -5,20 +5,22 @@ const plugin = require('../lib/index.js')
 const path = require('path')
 const {readFileSync} = require('fs')
 
-const fixture = file => readFileSync(path.join(__dirname, 'fixtures', `${file}.html`), 'utf8')
-const expected = file => readFileSync(path.join(__dirname, 'expected', `${file}.html`), 'utf8')
-
 // eslint-disable-next-line
 const error = (name, options, cb) => posthtml([plugin(options)]).process(fixture(name)).catch(cb)
 const clean = html => html.replace(/[^\S\r\n]+$/gm, '').trim()
 
+const fixture = file => readFileSync(path.join(__dirname, 'fixtures', `${file}.html`), 'utf8')
+const expected = file => readFileSync(path.join(__dirname, 'expected', `${file}.html`), 'utf8')
+
 const process = (t, name, options, log = false) => {
+  options = options || {url: 'https://example.com/'}
+
   return posthtml([plugin(options)])
     .process(fixture(name))
     .then(result => log ? console.log(result.html) : clean(result.html))
     .then(html => t.is(html, expected(name).trim()))
 }
 
-test('Basic', t => {
-  return process(t, 'basic')
+test('does nothing if url is not provided', t => {
+  return process(t, 'no-url', {})
 })
